4fcb67b0f2b206bc7cbf4e17836087a12b972830
Joey <jmullercuber@gmail.com>
2016-01-28 13:52:23 -0500

    Starting site with bootstrap Added bootstrap, random image on cover, using cellular automaton pics, templates for other pages

---
+++ cover.js
@@ -0,0 +1,14 @@
+// Load the same header on page
+$(function(){
+  $("#header").load("header.html");
+});
+
+// Helper function, getRandomIntInclusive
+// Taken from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function randNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Set a random background image
+// Thanks Arosboro at http://stackoverflow.com/a/5263171
+$(document.body).css('background-image', 'url("img/canvas' + randNum(1, 5) + '.png")');
